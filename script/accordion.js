d97d1a70033ee0e363835ec1f618ff810bfb04c7
Viktoria Gusakova <vgoosakova@gmail.com>
2021-05-18 21:29:45 +0300

    added accordion functionality
    Added when clicking on active accordion element, it should close

--- script/accordion.js
+++ script/accordion.js
@@ -2,23 +2,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const featureLinkElements = document.querySelectorAll('.feature__link');
     const featureSubElements = document.querySelectorAll('.feature-sub');
 
-    // for (let i = 0; i < featureLinkElements.length; i++) {
-    //     featureLinkElements[i].addEventListener('click', () => {
-    //         featureSubElements[i].classList.toggle('hidden');
-    //         featureLinkElements[i].classList.toggle('feature__link_active');
-    //     })
-    // }
-
     featureLinkElements.forEach((btn, index) => {
         btn.addEventListener('click', () => {
-            featureSubElements.forEach((featureSubElement) => {
-                featureSubElement.classList.add('hidden');
-            });
-            featureLinkElements.forEach((featureLinkElement) => {
-                featureLinkElement.classList.remove('feature__link_active');
-            });
-            featureSubElements[index].classList.remove('hidden');
-            btn.classList.add('feature__link_active');
+            if (!btn.classList.contains('feature__link_active')) {
+                featureSubElements.forEach((featureSubElement) => {
+                    featureSubElement.classList.add('hidden');
+                });
+                featureLinkElements.forEach((featureLinkElement) => {
+                    featureLinkElement.classList.remove('feature__link_active');
+                });
+                featureSubElements[index].classList.remove('hidden');
+                btn.classList.add('feature__link_active');
+            } else {
+                featureSubElements[index].classList.add('hidden');
+                btn.classList.remove('feature__link_active');
+            }
+            
+            // featureSubElements[index].classList.remove('hidden');
+            // btn.classList.add('feature__link_active');
         })
     })
 });
