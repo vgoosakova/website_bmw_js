8698055f6a59af4948ebcf79dfced536d8c519a2
Viktoria Gusakova <vgoosakova@gmail.com>
2021-05-19 17:17:13 +0300

    added smooth scrolling

---
+++ script/smoothScroll.js
@@ -0,0 +1,13 @@
+const smoothScrollElements = document.querySelectorAll('a[href^="#"]:not(a[href="#"])');
+
+smoothScrollElements.forEach(link => {
+    link.addEventListener('click', (event) => {
+
+        event.preventDefault();
+        const id = link.getAttribute('href').substring(1);
+
+        document.getElementById(id).scrollIntoView({
+            behavior: 'smooth'
+        });
+    })
+})
