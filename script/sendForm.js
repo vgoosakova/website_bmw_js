896339b74e6de21e3ea058188c1496b3fc292897
Viktoria Gusakova <vgoosakova@gmail.com>
2021-05-22 00:36:36 +0300

    added send forms

---
+++ script/sendForm.js
@@ -0,0 +1,72 @@
+const server = 'https://jsonplaceholder.typicode.com/posts';
+
+const sendData = (data, callBack, falseCallBack) => {
+    const request = new XMLHttpRequest();
+    request.open('POST', server);
+    
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState !== 4) return;
+        if (request.status === 200 || request.status === 201) {
+            const response = JSON.parse(request.responseText);
+            callBack(response.id);
+        } else {
+            falseCallBack(request.status)
+            throw new Error(request.status)
+        }
+    });
+
+    request.send(data)
+};
+
+const formElements = document.querySelectorAll('.form');
+
+const formHandler = (form) => {
+    const smallElement = document.createElement('small');
+    form.append(smallElement);
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const data = {};
+        let flag = true;
+        
+        const buttonSubmit = form.querySelector('.button[type = "submit"]');
+
+        for (const elem of form.elements) {
+            const { name, value } = elem
+            if (name) {
+                if (value.trim()) {
+                    elem.style.border = '';
+                    data[name] = value.trim();
+                } else {
+                    elem.style.border = '1px solid red';
+                    flag = false;
+                    elem.value = '';
+                }
+            }
+        }
+
+        if (!flag) {
+            return smallElement.textContent = 'Заполните все поля';
+        }
+
+        sendData(JSON.stringify(data),
+        (id) => {
+            smallElement.textContent = 'Ваша заявка №' + id +
+                 '. В ближайшее время с вами свяжемся!';
+            buttonSubmit.disabled = true;
+
+            setTimeout(() => {
+                smallElement.textContent = '';
+                buttonSubmit.disabled = false;
+            }, 5000)
+        },
+        () => {
+            smallElement.textContent = 'К сожалению технические неполадки, попробуйте отправить заявку позже';
+            smallElement.style.color = 'red';
+        });
+
+        form.reset();
+    })
+};
+
+formElements.forEach(formHandler)
