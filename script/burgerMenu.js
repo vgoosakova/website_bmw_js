3871c78fa61a614a9e6677c64d2f5fd40e598ed3
Viktoria Gusakova <vgoosakova@gmail.com>
2021-05-21 22:51:25 +0300

    update burger menu

--- script/burgerMenu.js
+++ script/burgerMenu.js
@@ -1,23 +1,26 @@
 const menuElement = document.querySelector('.menu');
 const burgerMenu = document.querySelector('.humburger-menu');
 
+const handlerMenu = event => {
+    const target = event.target;
+    const parent = target.closest('.menu');
+
+    if ((!parent && target !== burgerMenu) ||
+        target.classList.contains('menu-list__link')) {
+        toggleMenu()
+    }
+}
 
 const toggleMenu = () => {
     menuElement.classList.toggle('menu-active');
     burgerMenu.classList.toggle('humburger-menu-active');
-};
 
-const closeMenu = () => {
-    menuElement.classList.remove('menu-active');
-    burgerMenu.classList.remove('humburger-menu-active');
+
+    if (menuElement.classList.contains('menu-active')) {
+        document.body.addEventListener('click', handlerMenu)
+    } else {
+        document.body.removeEventListener('click', handlerMenu)
+    }
 };
 
 burgerMenu.addEventListener('click', toggleMenu);
-
-menuElement.addEventListener('click', (event) => {
-    const target = event.target;
-    
-    if (target.classList.contains('menu-list__link')) {
-        closeMenu();
-    }
-});
